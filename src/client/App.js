fbc348c4a6ca51193085304a7ff0ceeaa761dd34
blaze <kontaktAK@Yandex.ru>
2019-05-08 12:40:14 +0300

    adding some layouts

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Navbar } from './components/layout/Navbar'
+import { Landing } from './components/layout/Landing'
+import { Footer } from './components/layout/Footer'
 
 export class App extends Component {
   render () {
     return (
-      <div>
-        <h1>My Social Networks</h1>
+      <div className="App">
+        <Navbar/>
+        <Landing/>
+        <Footer/>
       </div>
     )
 
