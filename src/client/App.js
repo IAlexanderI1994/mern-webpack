6cb3417824b958426397933bdc3e9c6ea9823141
blaze <kontaktAK@Yandex.ru>
2019-05-16 11:23:24 +0300

    1) customize login and register pages 2) setting up redux

--- src/client/App.js
+++ src/client/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import { Navbar } from './components/layout/Navbar'
 import { Landing } from './components/layout/Landing'
 import { Footer } from './components/layout/Footer'
 import { Login } from './components/auth/Login'
 import { Register } from './components/auth/Register'
+import './App.css'
 
 export class App extends Component {
   render () {
     return (
-      <Router>
-        <div className="App">
-          <Navbar/>
-          <Route exact path="/" component={Landing}/>
-          <div className="container">
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/register" component={Register}/>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
+            <div className="container">
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/register" component={Register}/>
 
+            </div>
+            <Footer/>
           </div>
-          <Footer/>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
 
     )
 
